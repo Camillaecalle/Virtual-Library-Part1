ed3af7deda4ae1a31a17f6795ba3ea86751891eb
Camillaecalle <camilla7732528@gmail.com>
2023-02-08 15:25:13 -0500

    With comments

--- Book.cpp
+++ Book.cpp
@@ -1,5 +1,6 @@
 //Camilla Calle
 //Febuary 6, 2023
+//Book.cpp
 //Book.cpp is the implementation. Here I gave the functions (that I have in my interface file) 
 //functionality. 
 
@@ -7,48 +8,104 @@
 #include <iostream>
 #include <string> 
 
-//Default Constructor
-Book::Book() {   
+  /**
+      Default constructor.
+      Default-initializes all private members. Booleans are default-initialized to False.
+   */
+
+Book::Book() {
+    title_="";
+    author_="";
+    page_count_=0;
+    digital_ = false;
 }
-//Parameterized constructor
+
+/**
+      Parameterized constructor.
+      @param      : The title of the book (a string)
+      @param      : The author of the book (a string)
+      @param      : The number of pages in the book (a positive int)
+      @param      : A flag indicating whether the book is in digital form (a Boolean),
+                    with default value False
+      @post       : The private members are set to the values of the corresponding parameters.
+ */
+
 Book::Book(std::string title, std::string author, int pageCount, bool digital) {
     title_= title;
     author_= author;
-    pageCount_= pageCount;
+    page_count_= pageCount;
     digital_= digital;
 }
 
-const std::string Book::getTitle(){ //getting so no parameter
-    return title_; 
-}
+  /**
+    @param  : the title of the Book
+    @post   : sets the Book's title to the value of the parameter
+  */
+
 
 const void Book::setTitle(std::string title)  {
     title_ = title;
 }
 
-const std::string Book::getAuthor(){
-    return author_;
+  /**
+      @return : the title of the Book
+   */
+
+const std::string Book::getTitle(){ //getting so no parameter
+    return title_; 
 }
+
+   /**
+     @param  : the name of the author of the Book
+     @post   : sets the Book's author to the value of the parameter
+   */
 
 const void Book::setAuthor(std::string author) {
      author_ = author;
 }
 
-//setter for page count. (Mutator)
-//page count > 0
-//set page count to the value of the parameter. 
+   /**
+       @return : the author of the Book
+    */
+
+const std::string Book::getAuthor(){
+    return author_;
+}
+
+   /**
+     @param  : a positive integer page count
+     @pre    : page count > 0 - books cannot have a negative number of pages
+     @post   : sets the page count to the value of the parameter
+   */
+ 
 const void Book::setPageCount(int pageCount) { 
     if (pageCount > 0) {
-    pageCount_= pageCount;
+    page_count_= pageCount;
     }
 }
 
+   /**
+       @return : the page count
+    */
+
 const int Book::getPageCount() {
-    return pageCount_;
+    return page_count_;
 }
 
-// return true if the book is available in digital form, false ortherwise.
-// Accessor Function but written as isDigital NOT getDigital.
+
+   /**
+    @post   : sets the digital flag to true
+   */
+const void Book::setDigital() {
+    digital_= true;
+}
+
+  /**
+   @return true if the book is available in digital form, false otherwise
+
+   Note: this is an accessor function and must follow the same convention as all accessor functions even if it is not called getDigital
+  */
+
  bool Book::isDigital()const {
     if (digital_ == true){
         return true;
@@ -56,6 +113,3 @@ const int Book::getPageCount() {
     return false;
  }
 
-const void Book::setDigital() {
-    digital_= true;
-}
--- Book.hpp
+++ Book.hpp
@@ -1,5 +1,6 @@
 //Camilla Calle
 //Feburary 6, 2023
+//Book.hpp
 //Book.hpp is the interface. Here I created the class Book with the assigned public, private member variables, and public 
 //member functions. 
 
@@ -8,35 +9,99 @@
 
 #include <iostream>
 #include <string>
-//using namespace std;
 
+/*
+Created the Book class and declared the private member variables. 
+private:
+  - The title of the book (a string)
+  - The author of the book (a string)
+  - The page count (an integer)
+  - A flag indicating whether the book is available in digital form (a Boolean) */
 
-class Book {
+class Book { 
 private: 
 std::string title_;
 std::string author_; 
-int pageCount_;
+int page_count_;
 bool digital_;  
 
 public: 
-Book(); //Default Constructor
 
-//Parameterized Constructor
+  /**
+      Default constructor.
+      Default-initializes all private members. Booleans are default-initialized to False.
+   */
+
+Book(); 
+
+/**
+      Parameterized constructor.
+      @param      : The title of the book (a string)
+      @param      : The author of the book (a string)
+      @param      : The number of pages in the book (a positive int)
+      @param      : A flag indicating whether the book is in digital form (a Boolean),
+                    with default value False
+      @post       : The private members are set to the values of the corresponding parameters.
+ */
+
 Book(std::string title, std::string author, int pageCount, bool digital = false);
 
+  /**
+      @return : the title of the Book
+   */
 
 const std::string getTitle(); //getter
 
 
+  /**
+    @param  : the title of the Book
+    @post   : sets the Book's title to the value of the parameter
+  */
+
+
 const void setTitle(std::string title);//setter
 
+   /**
+       @return : the author of the Book
+    */
 
 const std::string getAuthor(); //getter
+
+   /**
+     @param  : the name of the author of the Book
+     @post   : sets the Book's author to the value of the parameter
+   */
+
 const void setAuthor(std::string author); //setter
+
+   /**
+     @param  : a positive integer page count
+     @pre    : page count > 0 - books cannot have a negative number of pages
+     @post   : sets the page count to the value of the parameter
+   */
+
 const void setPageCount(int pageCount); //setter
+
+
+   /**
+       @return : the page count
+    */
+
 const int getPageCount(); //getter
+
+   /**
+    @post   : sets the digital flag to true
+   */
+
 const void setDigital(); //setter
-bool isDigital() const;
+
+  /**
+   @return true if the book is available in digital form, false otherwise
+
+   Note: this is an accessor function and must follow the same convention as all accessor functions even if it is not called getDigital
+  */
+
+bool isDigital() const; //getter
 
 };
 #endif
